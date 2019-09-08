f88addb9c0f9dc67980a96ae964dd15736717ff1
Jameel Hamdan <jameelhamdan99@yahoo.com>
2019-09-08 14:16:58 +0300

    Update base.py

--- settings/base.py
+++ settings/base.py
@@ -1,5 +1,6 @@
 import os
 import neomodel.config as neo4j
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,3 +85,5 @@ LOGGING = {
         },
     },
 }
+
+django_heroku.settings(locals())
